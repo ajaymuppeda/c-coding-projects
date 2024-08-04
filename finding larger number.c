be7187b0e8589f65d0a811bd7f8b890cdaa12095
Ajay Muppeda <muppedaajay@gmail.com>
2024-08-04 17:30:37 +0530

    largernumber

---
+++ finding larger number.c
@@ -0,0 +1,23 @@
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main()  {
+    int a,b,c;
+    printf("Enter a numbers :");
+    scanf("%d",&a);
+    printf("Enter b numbers :");
+    scanf("%d",&b);
+    printf("Enter c numbers :");
+        scanf("%d",&c);
+        if(a>c)
+        if(a>b)
+        printf("%d larger value\n",a);
+        else
+        printf("%d larger value\n",b);
+    if(b>c)
+    printf("%d larger value\n",b);
+    else
+    printf("%d larger value\n",c);
+    return 0;
+}
