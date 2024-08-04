9b7a2dd57fd2fde7296561115b4e5bf610fd9514
Ajay Muppeda <muppedaajay@gmail.com>
2024-08-04 15:23:30 +0530

    adding

---
+++ addingtwonumbers.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main() 
+{
+    int a,b,c;
+    
+    printf("Enter a value:");
+    scanf("%d",&a);
+    printf("Enter b value:");
+    scanf("%d",&b);
+    c=a+b;
+    printf("answer:%d",c);
+    return 0;
+}
